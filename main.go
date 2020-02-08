6e7a7695d0e0466439654f258393719cae8116bf
yo8 <timixr@pm.me>
2020-02-08 12:31:54 +0800

    Let's rock

--- main.go
+++ main.go
@@ -7,17 +7,45 @@ import (
 )
 
 const (
-	chunkSizeKB  = 1024
-	chunkSizeMB  = 1024 * 1024
-	chunkSizeGB  = 1024 * 1024 * 1024
+	chunkSizeKB = 1024
+	chunkSizeMB = 1024 * 1024
+	chunkSizeGB = 1024 * 1024 * 1024
+
 	intervalTime = time.Second * 1
 	checkStep    = 50
+	maxStep      = 1024
+	stepSize     = chunkSizeGB
 )
 
 var (
 	totalSize = int64(0)
 	memCol    = make([][]byte, 0)
 )
+
+const (
+	sizeKB = 1 << ((iota + 1) * 10)
+	sizeMB
+	sizeGB
+	sizeTB
+	sizePB
+)
+
+func formatSize(size uint64) string {
+	switch sf := float64(size); {
+	case sf < sizeKB:
+		return fmt.Sprintf("%d B", size)
+	case sf < sizeMB:
+		return fmt.Sprintf("%.2f KiB", sf/sizeKB)
+	case sf < sizeGB:
+		return fmt.Sprintf("%.2f MiB", sf/sizeMB)
+	case sf < sizeTB:
+		return fmt.Sprintf("%.2f GiB", sf/sizeGB)
+	case sf < sizePB:
+		return fmt.Sprintf("%.2f TiB", sf/sizeTB)
+	default:
+		return fmt.Sprintf("%.2f PiB", sf/sizePB)
+	}
+}
 
 func allocMemory(size int64) {
 	memCol = append(memCol, make([]byte, 0, size))
@@ -27,18 +55,17 @@ func allocMemory(size int64) {
 func getMemoryStatus() {
 	var mem runtime.MemStats
 	runtime.ReadMemStats(&mem)
-	fmt.Printf("GC #%d, Sys: %.2f MiB, Alloc: %.2f MiB, Total: %.2f MiB\n",
+	fmt.Printf("GC #%d, Sys: %s, Alloc: %s, Total: %s\n",
 		mem.NumGC,
-		float64(mem.Sys)/1024/1024,
-		float64(mem.Alloc)/1024/1024,
-		float64(mem.TotalAlloc)/1024/1024,
+		formatSize(mem.Sys),
+		formatSize(mem.Alloc),
+		formatSize(mem.TotalAlloc),
 	)
 }
 
 func main() {
-	fmt.Println("Hello ❤ World!")
-	for i := 1; i <= 1024; i++ {
-		allocMemory(chunkSizeMB)
+	for i := 1; i <= maxStep; i++ {
+		allocMemory(stepSize)
 		fmt.Printf("done #%d - total: %d\n", i, totalSize)
 
 		if i%checkStep == 0 {
